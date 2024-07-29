4437d1684e960a84d8d8b9580158db00466509ca
Joaquim cassano <joaquimcassano@gmail.com>
2024-07-28 21:09:05 -0300

    Updated tutorials to the new version

--- tests.py
+++ tests.py
@@ -1,5 +1,14 @@
 import alertpix
 
-acc = alertpix.Account("Bearer 0deae4cb254bed51fef30a11a5c63b52")
 
-print(acc.wallet)
+token = "Bearer cb22b3ccb34d5ebc4e2ee25c32d2d015"
+client = alertpix.Alertpix(token)
+
+pagamento = client.Charge(link="apenasumnerdd", amount=1000, comment="Pagando aquele cafézinho", username="Remetente")
+pagamento.create()
+
+print("BrCode para o pagamento: ", pagamento.brcode.code)
+print("Aguardando pagamento...")
+while True:
+    if pagamento.paid == True:
+        print("Pagamento concluído!")
